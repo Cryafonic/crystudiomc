c248e3368c52af9e0a1ce90272c73ea321d9b49a
Brendon van der nest <brendonvandernest69@gmail.com>
2023-03-22 22:02:28 +0200

    Added a comment

--- src/main/java/net/crystudio/crystudiomod/CrystudioMod.java
+++ src/main/java/net/crystudio/crystudiomod/CrystudioMod.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 public class CrystudioMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("crystudiomod");
 
+	// this a comment
 	@Override
 	public void onInitialize() {
 
