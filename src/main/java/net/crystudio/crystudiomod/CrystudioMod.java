1e3f0182e5d12f310dcfb4743e518a9ce76f3696
Brendon van der nest <brendonvandernest69@gmail.com>
2023-03-23 18:18:50 +0200

    Added support for v1.19.4

--- src/main/java/net/crystudio/crystudiomod/CrystudioMod.java
+++ src/main/java/net/crystudio/crystudiomod/CrystudioMod.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 
 public class CrystudioMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("crystudiomod");
-
-	// this a comment
 	@Override
 	public void onInitialize() {
 
